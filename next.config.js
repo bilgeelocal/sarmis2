4c2cbe171d294d516dccfae7b1d09b9db05ea453
Bilegdemberel Tugbaatar <55517955+bilgee-cell@users.noreply.github.com>
2024-06-12 21:15:19 +0800

    Merged old repo into new repo

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    PUBLIC_BACKEND_API_URL: process.env.PUBLIC_BACKEND_API_URL,
-  },
-};
-
-module.exports = nextConfig;
